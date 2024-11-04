a77ae42630e67b753e5b10dd36f502e974fd7ae0
EuLey <wessoupra@hotmail.com>
2024-11-03 22:03:07 -0300

    adicionando ativos no BD com preço atual de mercado

--- carteira.py
+++ carteira.py
@@ -1,29 +1,37 @@
-# carteira.py
 import streamlit as st
 import pandas as pd
-import numpy as np
 import yfinance as yf
-import plotly.express as px
-from datetime import datetime
-from streamlit_extras.metric_cards import style_metric_cards
-from streamlit_extras.grid import grid
 from db_functions import adicionar_investimento, obter_investimentos_usuario
 
 def exibir_carteira_usuario(user_id):
     st.title("Sua Carteira de Investimentos")
 
     with st.form(key="form_investimento"):
-        
+        # Carregar os tickers disponíveis
         ativo_df = pd.read_csv("tickers_ibra.csv", index_col=0)
         ativo_selecionado = st.selectbox("Selecione a Empresa", options=ativo_df, help='Escolha o código da empresa')
-        ticker = f"{ativo_selecionado}" 
-        
+
         quantidade = st.number_input("Quantidade", min_value=1, step=1)
-        preco = st.number_input("Preço por Unidade", min_value=0.0, step=0.01)
+
+        # Verificar o preço atual do ativo
+        if ativo_selecionado:
+            ticker = f"{ativo_selecionado}.SA"  # Adicionar o sufixo para ativos da B3
+            ticker_data = yf.Ticker(ticker)
+            preco_data = ticker_data.history(period="1d")
+
+            # Validar se há dados disponíveis para o ativo
+            if not preco_data.empty:
+                preco = preco_data['Close'].iloc[-1]  # Preço de fechamento mais recente
+                st.write(f"Preço Atual do {ativo_selecionado}: R$ {preco:.2f}")
+            else:
+                st.write("Preço atual não disponível.")
+                preco = 0.0  # Preço padrão se não houver dados
+
         if st.form_submit_button("Adicionar Investimento"):
-            adicionar_investimento(user_id, ticker, quantidade, preco)
+            adicionar_investimento(user_id, ativo_selecionado, quantidade, preco)
             st.success("Investimento adicionado com sucesso!")
-    
+   
+    # Exibir os investimentos do usuário
     st.subheader("Seus Investimentos")
     investimentos = obter_investimentos_usuario(user_id)
     for inv in investimentos:
